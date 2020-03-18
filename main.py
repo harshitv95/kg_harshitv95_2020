686fa0c4ff2335204ee71f410da706b7d560a3c9
Harshit Vadodaria <harshitv95@gmail.com>
2020-03-18 19:52:54 -0400

    Implemented logic

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ MIN_ARGS_NUM = 2
 def main(argv):
     if len(argv) < (MIN_ARGS_NUM + 1):
         print("Please enter the {} arguments, and execute as:\n> python main.py arg1 arg2".format(MIN_ARGS_NUM))
+        return
     charmap = CharMap(argv[1])
     print(charmap.create_mapping(argv[2]))
 
